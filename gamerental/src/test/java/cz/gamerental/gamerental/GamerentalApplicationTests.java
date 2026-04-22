3f8c23f620a6382174bb43041c9477e3370f1df8
machajdaa <honzikmachace@seznam.cz>
2026-04-22 09:21:08 +0200

    Project base

---
+++ gamerental/src/test/java/cz/gamerental/gamerental/GamerentalApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.gamerental.gamerental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamerentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
